1153ea6c42a0705fefb9b050aa477f81dafed115
Justyn Henman <justynhenman@me.com>
2023-10-25 19:22:10 +0100

    Initial Commit

---
+++ FlipClock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlipClock
+//
+//  Created by Justyn Henman on 25/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlipClock/FlipClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipClockApp.swift
+//  FlipClock
+//
+//  Created by Justyn Henman on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlipClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
