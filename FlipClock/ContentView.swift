164afd83c9cdfa39f92b46286e55e2893b20461e
Justyn Henman <justynhenman@me.com>
2023-10-28 18:09:53 +0100

    feat: Modify time display on main page

--- FlipClock/ContentView.swift
+++ FlipClock/ContentView.swift
@@ -9,25 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var date: String = Date().formatted(date: .omitted,
-                                      time: .shortened)
+    @State private var hours: String = ""
+    @State private var minutes: String = ""
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        VStack {
-            Spacer()
-            Image(systemName: "globe")
-                .padding(.bottom)
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text(date)
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Spacer()
+        Group() {
+            VStack {
+                Spacer()
+                Text(hours)
+                    .font(.custom("Helvetica", size: 200))
+                    .fontWeight(.bold)
+                Text(minutes)
+                    .font(.custom("Helvetica", size: 200))
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            .onReceive(timer) { _ in
+                let components = Calendar.current.dateComponents([.hour, .minute], from: Date())
+                if let hour = components.hour, let minute = components.minute {
+                    hours = String(format: "%02d", hour)
+                    minutes = String(format: "%02d", minute)
+                }
         }
-        .padding()
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
