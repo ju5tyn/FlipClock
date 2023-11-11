44b2b4498d5cb33ad10a63b803a9a02401b5cd0e
Justyn Henman <justynhenman@me.com>
2023-11-11 13:05:29 +0000

    Revert "fix: update widget intent"
    This reverts commit 3ad22bb98cf1205a6958c515912060d04f0f7c06.

--- FlipClockWidget/AppIntent.swift
+++ FlipClockWidget/AppIntent.swift
@@ -10,5 +10,9 @@ import AppIntents
 
 struct ConfigurationAppIntent: WidgetConfigurationIntent {
     static var title: LocalizedStringResource = "Configuration"
-    static var description = IntentDescription("FlipClock widget")
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
 }
